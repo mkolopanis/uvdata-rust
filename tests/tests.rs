17771824f998b15c9672daeaccd33ce8e09038a2
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-09-24 16:22:04 -0700

    add some integration tests

---
+++ tests/tests.rs
@@ -0,0 +1,237 @@
+use ndarray::Array3;
+use num::Complex;
+use uvdata::*;
+
+#[test]
+fn init_metadata_false() {
+    let meta = UVMeta {
+        nbls: 3,
+        nblts: 15,
+        ntimes: 5,
+        nfreqs: 12,
+        npols: 4,
+        nspws: 1,
+        nants_data: 5,
+        nants_telescope: 12,
+        blt_order: BltOrder {
+            major: BltOrders::Unknown,
+            minor: BltOrders::Unknown,
+        },
+        phase_type: PhaseType::Drift,
+        vis_units: VisUnit::Jansky,
+        x_orientation: Orientation::Unknown,
+        instrument: "Test".to_owned(),
+        telescope_name: "Test".to_owned(),
+        telescope_location: [0.0, 0.0, 0.0],
+        eq_coeffs_convention: EqConvention::Unknown,
+        dut1: None,
+        gst0: None,
+        rdate: None,
+        earth_omega: None,
+        timesys: None,
+        uvplane_reference_time: None,
+        history: "".to_string(),
+    };
+    let test_data = Array3::<Complex<f64>>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_nsample = Array3::<f32>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_flag = Array3::<bool>::from_elem(
+        (
+            meta.nblts as usize,
+            meta.ntimes as usize,
+            meta.npols as usize,
+        ),
+        false,
+    );
+    let uvd = UVData::<f64, f32>::new(meta, false);
+    assert_eq!(uvd.data_array.unwrap(), test_data);
+    assert_eq!(uvd.nsample_array.unwrap(), test_nsample);
+    assert_eq!(uvd.flag_array.unwrap(), test_flag);
+}
+
+#[test]
+fn from_uvmeta_bool() {
+    let meta = UVMeta {
+        nbls: 3,
+        nblts: 15,
+        ntimes: 5,
+        nfreqs: 12,
+        npols: 4,
+        nspws: 1,
+        nants_data: 5,
+        nants_telescope: 12,
+        blt_order: BltOrder {
+            major: BltOrders::Unknown,
+            minor: BltOrders::Unknown,
+        },
+        phase_type: PhaseType::Drift,
+        vis_units: VisUnit::Jansky,
+        x_orientation: Orientation::Unknown,
+        instrument: "Test".to_owned(),
+        telescope_name: "Test".to_owned(),
+        telescope_location: [0.0, 0.0, 0.0],
+        eq_coeffs_convention: EqConvention::Unknown,
+        dut1: None,
+        gst0: None,
+        rdate: None,
+        earth_omega: None,
+        timesys: None,
+        uvplane_reference_time: None,
+        history: "".to_string(),
+    };
+    let test_data = Array3::<Complex<f64>>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_nsample = Array3::<f32>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_flag = Array3::<bool>::from_elem(
+        (
+            meta.nblts as usize,
+            meta.ntimes as usize,
+            meta.npols as usize,
+        ),
+        false,
+    );
+    let uvd = UVData::<f64, f32>::from((meta, false));
+    assert_eq!(uvd.data_array.unwrap(), test_data);
+    assert_eq!(uvd.nsample_array.unwrap(), test_nsample);
+    assert_eq!(uvd.flag_array.unwrap(), test_flag);
+}
+
+#[test]
+fn from_uvmeta() {
+    let meta = UVMeta {
+        nbls: 3,
+        nblts: 15,
+        ntimes: 5,
+        nfreqs: 12,
+        npols: 4,
+        nspws: 1,
+        nants_data: 5,
+        nants_telescope: 12,
+        blt_order: BltOrder {
+            major: BltOrders::Unknown,
+            minor: BltOrders::Unknown,
+        },
+        phase_type: PhaseType::Drift,
+        vis_units: VisUnit::Jansky,
+        x_orientation: Orientation::Unknown,
+        instrument: "Test".to_owned(),
+        telescope_name: "Test".to_owned(),
+        telescope_location: [0.0, 0.0, 0.0],
+        eq_coeffs_convention: EqConvention::Unknown,
+        dut1: None,
+        gst0: None,
+        rdate: None,
+        earth_omega: None,
+        timesys: None,
+        uvplane_reference_time: None,
+        history: "".to_string(),
+    };
+    let uvd = UVData::<f64, f32>::from(meta);
+    assert!(uvd.data_array.is_none());
+    assert!(uvd.nsample_array.is_none());
+    assert!(uvd.flag_array.is_none());
+}
+
+#[test]
+fn init_metadata_false_f32() {
+    let meta = UVMeta {
+        nbls: 3,
+        nblts: 15,
+        ntimes: 5,
+        nfreqs: 12,
+        npols: 4,
+        nspws: 1,
+        nants_data: 5,
+        nants_telescope: 12,
+        blt_order: BltOrder {
+            major: BltOrders::Unknown,
+            minor: BltOrders::Unknown,
+        },
+        phase_type: PhaseType::Drift,
+        vis_units: VisUnit::Jansky,
+        x_orientation: Orientation::Unknown,
+        instrument: "Foo".to_string(),
+        telescope_name: "Test".to_owned(),
+        telescope_location: [0.0, 0.0, 0.0],
+        eq_coeffs_convention: EqConvention::Unknown,
+        dut1: None,
+        gst0: None,
+        rdate: None,
+        earth_omega: None,
+        timesys: None,
+        uvplane_reference_time: None,
+        history: "".to_string(),
+    };
+    let test_data = Array3::<Complex<f32>>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_nsample = Array3::<f32>::zeros((
+        meta.nblts as usize,
+        meta.ntimes as usize,
+        meta.npols as usize,
+    ));
+    let test_flag = Array3::<bool>::from_elem(
+        (
+            meta.nblts as usize,
+            meta.ntimes as usize,
+            meta.npols as usize,
+        ),
+        false,
+    );
+    let uvd = UVData::<f32, f32>::new(meta, false);
+
+    assert_eq!(uvd.data_array.unwrap(), test_data);
+    assert_eq!(uvd.nsample_array.unwrap(), test_nsample);
+    assert_eq!(uvd.flag_array.unwrap(), test_flag);
+}
+#[test]
+fn init_metadata_true() {
+    let meta = UVMeta {
+        nbls: 3,
+        nblts: 15,
+        ntimes: 5,
+        nfreqs: 12,
+        npols: 4,
+        nspws: 1,
+        nants_data: 5,
+        nants_telescope: 12,
+        blt_order: BltOrder {
+            major: BltOrders::Unknown,
+            minor: BltOrders::Unknown,
+        },
+        phase_type: PhaseType::Drift,
+        vis_units: VisUnit::Jansky,
+        x_orientation: Orientation::Unknown,
+        instrument: "Test".to_owned(),
+        telescope_name: "Test".to_owned(),
+        telescope_location: [0.0, 0.0, 0.0],
+        eq_coeffs_convention: EqConvention::Unknown,
+        dut1: None,
+        gst0: None,
+        rdate: None,
+        earth_omega: None,
+        timesys: None,
+        uvplane_reference_time: None,
+        history: "".to_string(),
+    };
+    let uvd = UVData::<f64, f32>::new(meta, true);
+    assert!(uvd.data_array.is_none());
+    assert!(uvd.nsample_array.is_none());
+    assert!(uvd.flag_array.is_none());
+}
