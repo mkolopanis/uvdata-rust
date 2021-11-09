01d0a22f9607cfc96efa610b02665a7a15ebb3f6
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-11-09 14:20:00 -0700

    add approx dependency; impl partial eq and eq for meta data structs

--- src/base.rs
+++ src/base.rs
@@ -1,16 +1,17 @@
+use approx::abs_diff_eq;
 use ndarray::{Array, Ix1, Ix2};
 use serde::{Deserialize, Serialize};
 use std::{collections::BTreeMap, str::FromStr};
 
 // TODO: make and enum of the different catalog types
 // and catalog structs themselves probably too
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct UnphasedVal {
     pub cat_id: u32,
     pub cat_type: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SiderealVal {
     pub cat_id: u32,
     pub cat_type: String,
@@ -25,7 +26,104 @@ pub struct SiderealVal {
     pub info_source: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+impl PartialEq<SiderealVal> for SiderealVal {
+    fn eq(&self, other: &SiderealVal) -> bool {
+        match self.cat_id == other.cat_id {
+            true => {}
+            false => return false,
+        }
+
+        match self.cat_type == other.cat_type {
+            true => {}
+            false => return false,
+        }
+
+        match abs_diff_eq!(self.cat_lon, other.cat_lon, epsilon = 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match abs_diff_eq!(self.cat_lat, other.cat_lat, epsilon = 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.cat_frame == other.cat_frame {
+            true => {}
+            false => return false,
+        }
+
+        match abs_diff_eq!(self.cat_epoch, other.cat_epoch, epsilon = 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match &self.cat_pm_ra {
+            Some(pm_ra1) => match &other.cat_pm_ra {
+                Some(pm_ra2) => match abs_diff_eq!(pm_ra1, pm_ra2, epsilon = 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_pm_ra {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match &self.cat_pm_dec {
+            Some(pm_dec1) => match &other.cat_pm_dec {
+                Some(pm_dec2) => match abs_diff_eq!(pm_dec1, pm_dec2, epsilon = 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_pm_dec {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match &self.cat_dist {
+            Some(dist1) => match &other.cat_dist {
+                Some(dist2) => match abs_diff_eq!(dist1, dist2, epsilon = 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_dist {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match &self.cat_vrad {
+            Some(vrad1) => match &other.cat_vrad {
+                Some(vrad2) => match abs_diff_eq!(vrad1, vrad2, epsilon = 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_vrad {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match self.info_source == other.info_source {
+            true => {}
+            false => return false,
+        }
+        true
+    }
+}
+impl Eq for SiderealVal {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EphemVal {
     pub cat_id: u32,
     pub cat_type: String,
@@ -38,7 +136,74 @@ pub struct EphemVal {
     pub info_source: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+impl PartialEq<EphemVal> for EphemVal {
+    fn eq(&self, other: &EphemVal) -> bool {
+        match self.cat_id == other.cat_id {
+            true => {}
+            false => return false,
+        }
+        match self.cat_type == other.cat_type {
+            true => {}
+            false => return false,
+        }
+
+        match self.cat_lon.abs_diff_eq(&other.cat_lon, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.cat_lat.abs_diff_eq(&other.cat_lat, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.cat_frame == other.cat_frame {
+            true => {}
+            false => return false,
+        }
+
+        match abs_diff_eq!(self.cat_epoch, other.cat_epoch, epsilon = 1e-6) {
+            true => {}
+            false => return false,
+        }
+        match &self.cat_dist {
+            Some(dist1) => match &other.cat_dist {
+                Some(dist2) => match dist1.abs_diff_eq(dist2, 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_dist {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match &self.cat_vrad {
+            Some(vrad1) => match &other.cat_vrad {
+                Some(vrad2) => match vrad1.abs_diff_eq(vrad2, 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.cat_vrad {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match self.info_source == other.info_source {
+            true => {}
+            false => return false,
+        }
+        true
+    }
+}
+impl Eq for EphemVal {}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 #[serde(untagged)]
 pub enum CatTypes {
     Unphased(UnphasedVal),
@@ -265,7 +430,7 @@ impl FromStr for BltOrder {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub struct UVMeta {
     pub nbls: u32,
     pub nblts: u32,
@@ -294,6 +459,186 @@ pub struct UVMeta {
     pub history: String,
 }
 
+impl PartialEq<UVMeta> for UVMeta {
+    fn eq(&self, other: &UVMeta) -> bool {
+        match self.nbls == other.nbls {
+            true => {}
+            false => return false,
+        }
+
+        match self.nblts == other.nblts {
+            true => {}
+            false => return false,
+        }
+
+        match self.nspws == other.nspws {
+            true => {}
+            false => return false,
+        }
+
+        match self.npols == other.npols {
+            true => {}
+            false => return false,
+        }
+
+        match self.ntimes == other.ntimes {
+            true => {}
+            false => return false,
+        }
+
+        match self.nfreqs == other.nfreqs {
+            true => {}
+            false => return false,
+        }
+
+        match self.nphases == other.nphases {
+            true => {}
+            false => return false,
+        }
+
+        match self.nants_data == other.nants_data {
+            true => {}
+            false => return false,
+        }
+
+        match self.blt_order == other.blt_order {
+            true => {}
+            false => return false,
+        }
+
+        match self.vis_units == other.vis_units {
+            true => {}
+            false => return false,
+        }
+
+        match self.nants_telescope == other.nants_telescope {
+            true => {}
+            false => return false,
+        }
+
+        match self.phase_type == other.phase_type {
+            true => {}
+            false => return false,
+        }
+
+        match self.x_orientation == other.x_orientation {
+            true => {}
+            false => return false,
+        }
+
+        match self.instrument == other.instrument {
+            true => {}
+            false => return false,
+        }
+
+        match self.telescope_name == other.telescope_name {
+            true => {}
+            false => return false,
+        }
+
+        for (x1, x2) in self
+            .telescope_location
+            .iter()
+            .zip(other.telescope_location.iter())
+        {
+            match abs_diff_eq!(x1, x2, epsilon = 1e-6) {
+                true => {}
+                false => return false,
+            }
+        }
+
+        match self.object_name == other.object_name {
+            true => {}
+            false => return false,
+        }
+
+        match self.eq_coeffs_convention == other.eq_coeffs_convention {
+            true => {}
+            false => return false,
+        }
+
+        match self.dut1 {
+            Some(dut1) => match other.dut1 {
+                Some(dut2) => match abs_diff_eq!(dut1, dut2) {
+                    true => {}
+                    false => {
+                        return false;
+                    }
+                },
+                None => {
+                    return false;
+                }
+            },
+            None => match other.dut1 {
+                None => {}
+                Some(_) => {
+                    return false;
+                }
+            },
+        }
+
+        match self.gst0 {
+            Some(gst1) => match other.gst0 {
+                Some(gst2) => match abs_diff_eq!(gst1, gst2) {
+                    true => {}
+                    false => {
+                        return false;
+                    }
+                },
+                None => {
+                    return false;
+                }
+            },
+            None => match other.gst0 {
+                None => {}
+                Some(_) => {
+                    return false;
+                }
+            },
+        }
+
+        match self.rdate == other.rdate {
+            true => {}
+            false => return false,
+        }
+        // check handling here
+        match self.earth_omega {
+            Some(omega1) => match other.earth_omega {
+                Some(omega2) => match abs_diff_eq!(omega1, omega2) {
+                    true => {}
+                    false => {
+                        return false;
+                    }
+                },
+                None => {
+                    return false;
+                }
+            },
+            None => match other.earth_omega {
+                None => {}
+                Some(_) => {
+                    return false;
+                }
+            },
+        }
+
+        match self.timesys == other.timesys {
+            true => {}
+            false => return false,
+        }
+
+        match self.uvplane_reference_time == other.uvplane_reference_time {
+            true => {}
+            false => return false,
+        }
+
+        match self.history == other.history {
+            true => {}
+            false => return false,
+        }
+        true
+    }
+}
 impl Eq for UVMeta {}
 
 impl UVMeta {
@@ -337,7 +682,7 @@ impl Default for UVMeta {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub struct ArrayMetaData {
     pub spw_array: Array<u32, Ix1>,
     pub uvw_array: Array<f64, Ix2>,
@@ -358,6 +703,134 @@ pub struct ArrayMetaData {
     pub antenna_diameters: Option<Array<f32, Ix1>>,
     pub phase_center_catalog: Catalog,
     pub phase_center_id_array: Array<u32, Ix1>,
+}
+impl PartialEq<ArrayMetaData> for ArrayMetaData {
+    fn eq(&self, other: &ArrayMetaData) -> bool {
+        match self.spw_array == other.spw_array {
+            true => {}
+            false => return false,
+        }
+
+        match self.spw_array == other.spw_array {
+            true => {}
+            false => return false,
+        }
+        match self.uvw_array.abs_diff_eq(&other.uvw_array, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.time_array.abs_diff_eq(&other.time_array, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.lst_array.abs_diff_eq(&other.lst_array, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.ant_1_array == other.ant_1_array {
+            true => {}
+            false => return false,
+        }
+
+        match self.ant_2_array == other.ant_2_array {
+            true => {}
+            false => return false,
+        }
+
+        match self.baseline_array == other.baseline_array {
+            true => {}
+            false => return false,
+        }
+
+        match self.freq_array.abs_diff_eq(&other.freq_array, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.spw_id_array == other.spw_id_array {
+            true => {}
+            false => return false,
+        }
+
+        match self.polarization_array == other.polarization_array {
+            true => {}
+            false => return false,
+        }
+
+        match self
+            .integration_time
+            .abs_diff_eq(&other.integration_time, 1e-6)
+        {
+            true => {}
+            false => return false,
+        }
+
+        match self.channel_width.abs_diff_eq(&other.channel_width, 1e-6) {
+            true => {}
+            false => return false,
+        }
+
+        match self.antenna_numbers == other.antenna_numbers {
+            true => {}
+            false => return false,
+        }
+
+        match self.antenna_names == other.antenna_names {
+            true => {}
+            false => return false,
+        }
+
+        match self
+            .antenna_positions
+            .abs_diff_eq(&other.antenna_positions, 1e-6)
+        {
+            true => {}
+            false => return false,
+        }
+
+        match &self.eq_coeffs {
+            Some(coeffs1) => match &other.eq_coeffs {
+                Some(coeffs2) => match coeffs1.abs_diff_eq(coeffs2, 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.eq_coeffs {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match &self.antenna_diameters {
+            Some(diams1) => match &other.antenna_diameters {
+                Some(diams2) => match diams1.abs_diff_eq(diams2, 1e-6) {
+                    true => {}
+                    false => return false,
+                },
+                None => return false,
+            },
+            None => match &other.antenna_diameters {
+                None => {}
+                Some(_) => return false,
+            },
+        }
+
+        match self.phase_center_catalog == other.phase_center_catalog {
+            true => {}
+            false => return false,
+        }
+
+        match self.phase_center_id_array == other.phase_center_id_array {
+            true => {}
+            false => return false,
+        }
+
+        true
+    }
 }
 impl Eq for ArrayMetaData {}
 
